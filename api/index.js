c0ffa9fe6829fe7912330fae82534856698df09e
Mustago <muga200301@gmail.com>
2025-04-05 09:58:30 +0500

    v.1.2

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const { sql } = require("@vercel/postgres");
@@ -8,9 +8,26 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 // Create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+app.use(express.json());
 app.use(express.static("public"));
+
+// для cors ========
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173", // Разрешить только этот домен
+//     methods: "GET,POST,PUT,DELETE",
+//     allowedHeaders: "Content-Type",
+//   })
+// );
+app.use(
+  cors({
+    origin: "https://frontend-plants.vercel.app/", // Разрешить только этот домен
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type",
+  })
+);
+// =================
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "..", "components", "home.html"));
@@ -25,92 +42,228 @@ app.get("/uploadUser", function (req, res) {
     path.join(__dirname, "..", "components", "user_upload_form.html")
   );
 });
-
-app.post("/uploadSuccessful", urlencodedParser, async (req, res) => {
-  try {
-    await sql`CREATE TABLE plants(id SERIAL PRIMARY KEY,
-    name VARCHAR(200) NOT NULL,
-    latin_name VARCHAR NOT NULL,
-    description TEXT NOT NULL
-)`;
-    res.status(200).send("<h1>User added successfully</h1>");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error adding user");
-  }
+app.get("/addnew", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "components", "addplants.html"));
 });
 
-app.get("/allUsers", async (req, res) => {
+// получение всех список расение
+app.get("/plants", async (req, res) => {
   try {
-    const users = await sql`SELECT * FROM plants;`;
-    if (users && users.rows.length > 0) {
-      let tableContent = users.rows
-        .map(
-          (plant) =>
-            `<tr>
-                        <td>${plant.id}</td>
-                        <td>${plant.name}</td>
-                        <td>${plant.latin_name}</td>
-                        <td>${plant.description}</td>
-                    </tr>`
-        )
-        .join("");
-
-      res.status(200).send(`
-                <html>
-                    <head>
-                        <title>Users</title>
-                        <style>
-                            body {
-                                font-family: Arial, sans-serif;
-                            }
-                            table {
-                                width: 100%;
-                                border-collapse: collapse;
-                                margin-bottom: 15px;
-                            }
-                            th, td {
-                                border: 1px solid #ddd;
-                                padding: 8px;
-                                text-align: left;
-                            }
-                            th {
-                                background-color: #f2f2f2;
-                            }
-                            a {
-                                text-decoration: none;
-                                color: #0a16f7;
-                                margin: 15px;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <h1>Users</h1>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>User ID</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                ${tableContent}
-                            </tbody>
-                        </table>
-                        <div>
-                            <a href="/">Home</a>
-                            <a href="/uploadUser">Add User</a>
-                        </div>
-                    </body>
-                </html>
-            `);
+    const plants = await sql`SELECT * FROM plants;`;
+    if (plants && plants.rows.length > 0) {
+      res.status(200).json({ message: "Ok", data: plants.rows });
     } else {
-      res.status(404).send("Users not found");
+      res.status(404).json({ message: "Растение не существует" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send("Error retrieving users");
+    res.status(500).json({ message: "Ошибка при получение данних из БД" });
+  }
+});
+// для добавление новый запис
+// app.post("/plants", urlencodedParser, async (req, res) => {
+//   try {
+//     console.log(req.body);
+//     await sql`INSERT INTO plants (name, latin_name, description) VALUES (${req.body.name}, ${req.body.latin_name}, ${req.body.description});`;
+//     res.status(200).send("<h1>User added successfully</h1>");
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Error adding user");
+//   }
+// });
+app.post("/addPlant", urlencodedParser, async (req, res) => {
+  try {
+    console.log(req.body);
+    req.body.name = req.body.name.replace(/[\x00-\x1F\x7F]/g, "");
+    req.body.latin_name = req.body.latin_name.replace(/[\x00-\x1F\x7F]/g, "");
+    req.body.description = req.body.description.replace(/[\x00-\x1F\x7F]/g, "");
+    req.body.habitat = req.body.habitat.replace(/[\x00-\x1F\x7F]/g, "");
+    req.body.medicinal_uses = req.body.medicinal_uses.replace(
+      /[\x00-\x1F\x7F]/g,
+      ""
+    );
+    await sql`INSERT INTO plants  (name,latin_name, family_id,  description, habitat, medicinal_uses, image) VALUES (${
+      req.body.name
+    }, ${req.body.latin_name}, ${Number(req.body.family_id)}, ${
+      req.body.description
+    },  ${req.body.habitat} , ${req.body.medicinal_uses}, ${req.body.image});`;
+    res.status(200).send(`
+      <div>
+        <h2>Растение ${req.body.name} успешно добавлена на БД.</h2>
+        <a href='/addnew'>Вернутся на страница добавление</a>
+      `);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Ошибка при добавление");
+  }
+});
+
+// добавление семейство
+app.post("/addFamily", urlencodedParser, async (req, res) => {
+  try {
+    console.log(req.body);
+    req.body.family_name = req.body.family_name.replace(/[\x00-\x1F\x7F]/g, "");
+    req.body.family_description = req.body.family_description.replace(
+      /[\x00-\x1F\x7F]/g,
+      ""
+    );
+    await sql`INSERT INTO families  (name, description) VALUES (${req.body.family_name}, ${req.body.family_description});`;
+    res.status(200).send(`
+      <div>
+        <h2>Семейство ${req.body.family_name} успешно добавлена на БД.</h2>
+        <a href='/addnew'>Вернутся на страница добавление</a>
+      `);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Ошибка при добавление нового семейство");
+  }
+});
+
+// список семейств families
+app.get("/families", async (req, res) => {
+  try {
+    const families = await sql`SELECT * FROM families;`;
+    if (families && families.rows.length > 0) {
+      res.status(200).json({ message: "Ok", data: families.rows });
+    } else {
+      res.status(404).json({ message: "families не существует" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Ошибка при получение данних из БД" });
+  }
+});
+
+// место произрастания
+app.get("/locations", async (req, res) => {
+  try {
+    const region = await sql`SELECT * FROM locations;`;
+    if (region && region.rows.length > 0) {
+      res.status(200).json({ message: "Ok", data: region.rows });
+    } else {
+      res.status(404).json({ message: "locations пустой" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Ошибка при получение данних из БД" });
+  }
+});
+// добавление место произрастение растения
+app.post("/location", urlencodedParser, async (req, res) => {
+  try {
+    console.log(req.body);
+    req.body.region = req.body.region.replace(/[\x00-\x1F\x7F]/g, "");
+    req.body.location_description = req.body.location_description.replace(
+      /[\x00-\x1F\x7F]/g,
+      ""
+    );
+
+    await sql`INSERT INTO locations (region, description) VALUES (${req.body.region}, ${req.body.location_description});`;
+    res.status(200).send(`
+      <div>
+        <h2>Место произрастение ${req.body.region} успешно добавлена на БД.</h2>
+        <a href='/addnew'>Вернутся на страница добавление</a>
+      `);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Ошибка при добавление нового семейство");
+  }
+});
+
+//  химическое соединение
+app.get("/chem_comps", async (req, res) => {
+  try {
+    const chem_comp = await sql`SELECT * FROM chemical_compounds;`;
+    if (chem_comp && chem_comp.rows.length > 0) {
+      res.status(200).json({ message: "Ok", data: chem_comp.rows });
+    } else {
+      res.status(404).json({ message: "chemical_compounds пустой" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Ошибка при получение данних из БД" });
+  }
+});
+// добавление химическое соединение
+app.post("/chem_comp", urlencodedParser, async (req, res) => {
+  try {
+    console.log(req.body);
+    req.body.chemical_name = req.body.chemical_name.replace(
+      /[\x00-\x1F\x7F]/g,
+      ""
+    );
+    req.body.chemical_description = req.body.chemical_description.replace(
+      /[\x00-\x1F\x7F]/g,
+      ""
+    );
+
+    await sql`INSERT INTO chemical_compounds (name, description) VALUES (${req.body.chemical_name}, ${req.body.chemical_description});`;
+    res.status(200).send(`
+      <div>
+        <h2>Химическое соединение ${req.body.chemical_name} успешно добавлена на БД.</h2>
+        <a href='/addnew'>Вернутся на страница добавление</a>
+      `);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Ошибка при добавление нового семейство");
+  }
+});
+
+// Связать растение с местом произрастания
+app.post("/plant_location", urlencodedParser, async (req, res) => {
+  try {
+    console.log(req.body);
+    const { plant_id, location_id } = req.body;
+    const rez = await sql`select plantlocation(${Number(plant_id)}, ${Number(
+      location_id
+    )});`;
+    res.status(200).send(`
+      <div>
+        <h2>Связать растение ${plant_id} с местом произрастания ${location_id} ${rez.rows[0].plantlocation}.</h2>
+        <a href='/addnew'>Вернутся на страница добавление</a>
+      `);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Ошибка при добавление нового семейство");
+  }
+});
+
+// Связать растение с химическим соединением
+app.post("/plant_chemical", urlencodedParser, async (req, res) => {
+  try {
+    console.log(req.body);
+    const { plant_id, chemical_id } = req.body;
+
+    const rez = await sql`select plantchemical(${Number(plant_id)}, ${Number(
+      chemical_id
+    )})`;
+
+    console.log(rez.rows);
+
+    res.status(200).send(`
+      <div>
+        <h2>Связать растение ${plant_id} с химическим соединением ${chemical_id} ${rez.rows[0].plantchemical}.</h2>
+        <a href='/addnew'>Вернутся на страница добавление</a>
+      `);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Ошибка при добавление нового семейство");
+  }
+});
+
+app.get("/getplant/:id", async (req, res) => {
+  try {
+    const plant = await sql`SELECT getplant(${Number(req.params.id)});`;
+    console.log(plant.rows.length, plant.rows);
+
+    if (plant && plant.rows.length > 0) {
+      res.status(200).json(JSON.parse(plant.rows[0].getplant));
+    } else {
+      res.status(404).json({ message: "plant пустой" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Ошибка при получение данних из БД" });
   }
 });
 
